a9004db5af9db3eb3fa48787ddba8b9be38213d4
Yash97223 <yashprajapati97223@gmail.com>
2025-03-07 14:54:13 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo chown ubuntu:ubuntu /home/ubuntu/blogprojectdrf/
+
 tmux new-session -s backend
 tmux send-keys -t backend "cd /home/ubuntu/blogprojectdrf/" C-m
 tmux send-keys -t backend "./filebrowser" C-m
